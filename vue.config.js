67991d83ff427d0ad0c5942ec17afde73524aaed
Akila Madhushanka <developer.akila5@gmail.com>
2025-03-22 21:04:48 +0530

    change improve

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,51 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/dist/',
+  pwa: {
+    name: 'My Vue PWA',
+    themeColor: '#42b983',
+    msTileColor: '#000000',
+    workboxPluginMode: 'GenerateSW', // Generates a service worker automatically
+    workboxOptions: {
+      clientsClaim: true, // Controls active clients without a refresh
+      skipWaiting: true, // Forces updates immediately
+      offlineGoogleAnalytics: true, // Enables offline tracking for Google Analytics
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+            },
+          },
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|ico)$/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'image-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+            },
+          },
+        },
+        {
+          urlPattern: /\.(?:js|css|json|html)$/i,
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'static-resources',
+            expiration: {
+              maxEntries: 30,
+              maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+            },
+          },
+        },
+      ],
+    },
+  },
 })
